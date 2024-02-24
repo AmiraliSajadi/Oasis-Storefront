87c90493144f61dd0a31c0ce078bf18ae7d4808e
AmiraliSajadi <amirali.sajadi@outlook.com>
2024-02-24 14:29:36 -0500

    added the missing email_validator package to requirements.txt

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField;
 from wtforms.validators import DataRequired, Length, Email, EqualTo;
+from email_validator import validate_email, EmailNotValidError
 
 
 class RegistrationForm(FlaskForm):
