364fedd875fcbe8db99a1370b48db0f78a0ff0fe
AmiraliSajadi <amirali.sajadi@outlook.com>
2024-02-24 14:30:54 -0500

    small dependency fix

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField;
 from wtforms.validators import DataRequired, Length, Email, EqualTo;
-from email_validator import validate_email, EmailNotValidError
 
 
 class RegistrationForm(FlaskForm):
