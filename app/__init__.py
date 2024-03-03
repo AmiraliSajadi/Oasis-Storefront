171c3dbcd8eed5f2948d59d9f3260e7bc85aab04
AmiraliSajadi <sajadi.amirali.sa@gmail.com>
2024-03-02 22:01:47 -0500

    finally created a basic user

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,18 @@
-
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-
-# with app.app_context():
-#     db.create_all()
-
-# app.config.from_object(Config)
+from app import models
+# Create database tables if they don't exist
+with app.app_context():
+    try:
+        db.create_all()
+        print("Database tables created successfully.")
+    except Exception as e:
+        print("An error occurred while creating database tables:", str(e))
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from app import app, db, bcrypt
-from app.models import User, Product, Category, Rating
+from app import app, db
+from flask_bcrypt import Bcrypt
+from app.models import MyUser
 from app.forms import RegistrationForm, LoginForm
 from flask import render_template, redirect, url_for, flash, session, request
 
@@ -19,12 +20,21 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.Username.data, email=form.Email.data, password=hashed_password)
+        user = MyUser(username=form.Username.data, email=form.Email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to login', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route("/create_user")
+def create_user():
+    # Create a new user
+    new_user = MyUser(username='example_user', email='user@example.com')
+    db.session.add(new_user)
+    db.session.commit()
+    flash('New user created successfully!', 'success')
+    return redirect(url_for('home'))
 
 # @app.route('/logout')
 # @login_required
@@ -59,6 +69,3 @@ def contact():
 @app.route("/products")
 def products():
     return render_template('products.html', title='Products')
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
