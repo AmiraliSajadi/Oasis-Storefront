394ef283512fb97c12e6d3d8c3cbb626fc1ac8fa
Dhrumil Patel <dhrumilpatel75319@gmail.com>
2024-03-04 12:34:09 -0500

    Added slider images to main.html page

--- app/static/js/modal.js
+++ app/static/js/modal.js
@@ -1,20 +1,60 @@
 $(document).ready(function () {
-    // Show Sign In Modal
-    $('#signInLink').click(function () {
-      $('#signInModal').modal('show');
-    });
-
-    // Show Sign Up Modal
-    $('#showSignUp').click(function () {
-      $('#signInModal').modal('hide');
-      $('#signUpModal').modal('show');
-    });
+  // Show Sign In Modal
+  $('#signInLink').click(function () {
+    $('#signInModal').modal('show');
   });
+
+  // Show Sign Up Modal
+  $('#showSignUp').click(function () {
+    $('#signInModal').modal('hide');
+    $('#signUpModal').modal('show');
+  });
+});
+
+
+// Image slideshow
+var slideIndex = 1;  // Initialize slideIndex to 1
+showSlides(slideIndex);  // Call showSlides with slideIndex to show the first slide
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  slides[slideIndex-1].style.display = "block";  
+}
+
+// Auto Slide
+function autoSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(autoSlides, 3000); // Change image every 3 seconds
+}
+
+autoSlides(); // Start the automatic slideshow
+
 
 
 
 // navbar toggle
- 
+
 
 const overlay = document.querySelector("[data-overlay]");
 const navOpenBtn = document.querySelector("[data-nav-open-btn]");
@@ -37,6 +77,6 @@ for (let i = 0; i < navElemArr.length; i++) {
 const header = document.querySelector("[data-header]");
 
 window.addEventListener("scroll", function () {
- window.scrollY >= 200 ? header.classList.add("active")
-   : header.classList.remove("active");
+  window.scrollY >= 200 ? header.classList.add("active")
+    : header.classList.remove("active");
 })
