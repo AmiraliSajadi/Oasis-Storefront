d2000fa317370dd67fd4d0c4a334422941e7ca2d
AmiraliSajadi <sajadi.amirali.sa@gmail.com>
2024-03-07 17:41:21 -0500

    added tables for wishlist & product sellers

--- app/models.py
+++ app/models.py
@@ -1,28 +1,25 @@
 from app import db
 
-# class User(db.Model):
-#     __tablename__ = 'user'  # You might need to change this if 'user' is a reserved keyword
-#     __table_args__ = {'schema': 'public'}  # Specify the schema explicitly
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(64), index=True, unique=True)
-#     email = db.Column(db.String(120), index=True, unique=True)
-#     # products = db.relationship('Product', backref='seller', lazy='dynamic')
-
-#     def __repr__(self):
-#         return '<User {}>'.format(self.username)
-class MyUser(db.Model):
-    __tablename__ = 'my_user'
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    # products = db.relationship('Product', backref='seller', lazy='dynamic')
+    wishlist = db.relationship('Wishlist', backref='user', lazy='dynamic')
 
     def __repr__(self):
-        return '<MyUser {}>'.format(self.username)
+        return '<User {}>'.format(self.username)
+
+class Wishlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+
+    def __repr__(self):
+        return '<Wishlist {}>'.format(self.id)
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)  # Make category name unique
+    name = db.Column(db.String(64), index=True, unique=True)
     description = db.Column(db.Text)
     products = db.relationship('Product', backref='category', lazy='dynamic')
 
@@ -31,6 +28,7 @@ class Category(db.Model):
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     quantity = db.Column(db.Integer)
     name = db.Column(db.String(128))
@@ -39,15 +37,8 @@ class Product(db.Model):
     full_description = db.Column(db.Text)
     image_url = db.Column(db.String(256))
 
+    seller = db.relationship('User', backref='products', lazy='select')
+
     def __repr__(self):
         return '<Product {}>'.format(self.id)
 
-# class Rating(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     rating = db.Column(db.Integer)
-#     review = db.Column(db.Text)
-#     product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-#     def __repr__(self):
-#         return '<Rating {}>'.format(self.rating)
